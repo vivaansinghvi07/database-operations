f1a1f511992e5e76cb6089a7d56f1bd4bd4341cc
Vivaan Singhvi <vsinghvi@dev-db1.adc.arm.gov>
2023-06-09 13:21:24 +0000

    added commnent to file

--- database/primary_key.sql
+++ database/primary_key.sql
@@ -1,3 +1,4 @@
+-- Gets the primary keys of a table given its schema and its name
 SELECT DISTINCT ccu.column_name  
 FROM information_schema.constraint_column_usage as ccu
 JOIN information_schema.table_constraints tc ON tc.constraint_name = ccu.constraint_name
